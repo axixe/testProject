5817bf0943b086a6e107a95584fe59afeae2ee60
Alexander <s1ngleez@yandex.ru>
2024-07-19 09:55:56 +0300

    first lesson

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('hello world');
+const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", ""),
+      personalMovieDB = {
+          count: numberOfFilms,
+          movies: {},
+          actors: {},
+          genres: [],
+          privat: false
+      };
+
+let film = prompt('Один из последних просмотренных фильмов?', ''),
+    evaluation = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies = {
+    [film]: evaluation
+};
+
+console.log(personalMovieDB);
